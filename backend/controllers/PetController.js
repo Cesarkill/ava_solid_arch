adf9e93574b8de485d2d7208753f40eee9b55417
Cesarkill <cesargiacomino270@gmail.com>
2026-05-13 00:49:19 -0300

    Task 5: Busca e Remoção de Pet por ID (getPetById e removePetById)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -122,4 +122,52 @@ module.exports = class PetController {
         res.status(200).json({message: 'em construçãoo'});
         return;
     }
-}
+    static async getPetById(req, res) {
+        const id = req.params.id;
+
+        if (!mongoose.types.objectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' });
+            return;
+        }
+        try {
+            const pet = await pet.findById(id);
+            if (!pet) {
+                res.status(404).json({ message: 'Pet não encontrado!' });
+                return;
+            }
+            res.status(200).json({ success: true, data: pet });
+        } catch (error) {
+            res.status(500).json({ message: error });
+        }
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' });
+            return;
+        }
+        res.status(200).json({data: pet });
+    }
+    static async removePetbyid(req, res) {
+        const id = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' });
+            return;
+        }
+
+        const pet = await pet.findById(id);
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' });
+            return;
+        }
+
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+        if (pet.user._id.toString() !== user._id.toString()) {
+            res.status(403).json({ message: 'Apenas o dono do pet pode removê-lo!' });
+            return;
+        }
+        await pet.findByIdAndDelete(id);
+        res.status(200).json({ message: 'Pet removido com sucesso!', data: pet });
+    }
+    
+    }
