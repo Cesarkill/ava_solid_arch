31b435af6dd4737851b8103e801897ca87495d68
Cesarkill <cesargiacomino270@gmail.com>
2026-05-13 01:13:13 -0300

    Task 7: Agendamento e Conclusão de Adoção (schedule e concludeAdoption)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -8,6 +8,7 @@ const getToken = require('../helpers/get-Token');
 const getUserByToken = require('../helpers/get-User-By-Token');
 const { countDocuments } = require('../models/Pet');
 const { use } = require('react');
+const Pet = require('../models/Pet');
 
 module.exports = class PetController {
     static async create(req, res) {
@@ -169,5 +170,69 @@ module.exports = class PetController {
         await pet.findByIdAndDelete(id);
         res.status(200).json({ message: 'Pet removido com sucesso!', data: pet });
     }
-    
+    static async schedule(req, res) {
+        const id = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' });
+            return;
+        }
+        const pet = await pet.findById(id);
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' });
+            return;
+        }
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        if (pet.user._id.toString() === user._id.toString()) {
+            res.status(403).json({ message: 'Você não pode agendar uma visita com seu próprio pet!' });
+            return;
+        }
+
+        pet.adopter = {
+            _id: user._id,
+            name: user.name,
+            image: user.image,
+            phone: user.phone,
+        };
+
+        try {
+            await Pet.findByIdAndUpdate(id, pet);
+            res.status(200).json({ message: 'Visita agendada com sucesso!' });
+        }catch (error) {
+            res.status(503).json({ message: error });
+        }
     }
+    static async concludeAdoption(req, res) {
+        const id = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' });
+            return;
+        }
+
+        const pet = await pet.findById(id);
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' });
+            return;
+        }
+         const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        if (pet.user._id.toString() !== user._id.toString()) {
+            res.status(403).json({ message: 'Apenas o dono do pet pode concluir a adoção!' });
+            return;
+        }
+        pet.available = false;
+        
+        try {
+            await Pet.findByIdAndUpdate(id, pet);
+            return res.status(200).json({ message: 'A adoção foi concluída com sucesso!' });
+        }catch (error) {
+            res.status(503).json({ message: error });
+        }
+    }
+    }
+
